a94a9de75792b86b1542ba02c329986a33298cb1
excavator <str_token@mail.ru>
2019-11-25 16:38:10 +0300

    redirect and err 404

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import { Route, NavLink, Switch } from 'react-router-dom'
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom'
 import './App.scss'
 import About from './About/About'
 import Cars from './Cars/Cars'
@@ -42,7 +42,14 @@ class App extends Component {
           <Route path="/" exact render = {() => <h1>Home Page</h1>} />
           <Route path="/about" component = {About} />
           <Route path="/cars/:name" component={CarDatail} />
-          <Route path="/cars" component = {Cars} />              
+          <Route path="/cars" component = {Cars} />
+          <Redirect to = {'/'}/>
+          {/* <Route 
+            render={()=><h1 style={{
+              color: 'red',
+              textAlign: 'center'
+            }}>404 not found</h1>}
+          />               */}
         </Switch>  
       </div>
     );
